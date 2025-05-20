33737dff3d82b360414fceb9fd48e6e8308e2970
Артур Соколов <123kapefa@gmail.com>
2025-05-20 14:14:03 +0300

    Создана страница входа.

--- app/src/main/java/com/example/topacademy_android/MainActivity.kt
+++ app/src/main/java/com/example/topacademy_android/MainActivity.kt
@@ -2,31 +2,26 @@ package com.example.topacademy_android
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.topacademy_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
-        private const val ON_CREATE = "ON_CREATE"
-    }
-
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val button = findViewById<Button>(R.id.btnStart)
-        button.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
-
-        Log.i(ON_CREATE, "Активити создана!")
     }
 
     override fun onStart() {
